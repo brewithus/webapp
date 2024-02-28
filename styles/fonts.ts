baa278738f47a549efe5fb32a6dd70fe65e5db0f
bluesimp1102 <jackismyname7@gmail.com>
2024-02-28 15:07:07 -0800

    #12 fonts, change color themes

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,6 +1,13 @@
-import { Poppins, Rubik } from 'next/font/google';
+import { Caveat, Poppins, Rubik } from 'next/font/google';
 
 export const rubikFont = Rubik({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const bigTitleFont = Caveat({
   weight: ['400', '500', '600', '700'],
   style: ['normal'],
   subsets: ['latin'],
