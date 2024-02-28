62a5e136fd0a11f3eedfb95f4e89dd9b07c25ba5
bluesimp1102 <jackismyname7@gmail.com>
2024-02-28 15:07:35 -0800

    #12 docs

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,7 @@ import { twMerge } from 'tailwind-merge';
  * Combines class names using clsx and then merges them with tailwind-merge.
  * This function allows for dynamic class name generation with conditional
  * and grouped class names support, optimized for Tailwind CSS.
+ * This function is mostly used by `shadcn/ui` components.
  * @param inputs - An array of class values to be combined and merged.
  * @returns The merged class names as a single string.
  */
