247bd7628934824acb91793940da9f3aaac5295a
bluesimp1102 <jackismyname7@gmail.com>
2024-02-29 18:03:26 -0800

    #19 coffee shop search bar
    Added a CTA button and search bar in the Hero Section:
    - allow user to select a location by entering the address with google map api
    - allow user to select tags (dummy tags)
    - on button click, user will be redirect to coffee spots search view at `/search?location=&tags=`

---
+++ components/find-coffee/FindCoffeeSpot.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import React, { useState } from 'react';
+import MultiSelectCombobox from './TagsSelection';
+import PlacesAutocomplete from './PlacesAutocomplete';
+import { Separator } from '../ui/separator';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
+
+const FindCoffeeSpot: React.FC<{ className?: string }> = ({
+  className,
+}): JSX.Element => {
+  const router = useRouter();
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  // state to store the selected address details
+  const [selectedAddress, setSelectedAddress] = useState<string>('');
+
+  // Define the handleSelectAddress function
+  const handleSelectAddress = (address: string): void => {
+    // Update the state with the new address and its lat/lng
+    setSelectedAddress(address);
+  };
+
+  const handleFindCoffeeSpotsClick = (): void => {
+    if (!selectedAddress) {
+      // Alert the user to select a location first
+      alert('Please select a location first.');
+      return;
+    }
+
+    // Construct the query params string
+    const queryParams = new URLSearchParams({
+      address: selectedAddress,
+      tags: selectedTags.join(','),
+    }).toString();
+
+    // Redirect to the /search page with query params
+    router.push(`/search?${queryParams}`);
+  };
+
+  return (
+    <div className={cn('w-full flex flex-col items-center gap-4', className)}>
+      <div className="flex flex-row gap-3 items-center bg-white/80 rounded-lg px-4 w-full h-fit">
+        <div className="flex-1 w-max">
+          <MultiSelectCombobox
+            selectedTags={selectedTags}
+            onTagsChange={setSelectedTags}
+          />
+        </div>
+        <Separator orientation="vertical" className="h-3/4" />
+        <PlacesAutocomplete onSelectAddress={handleSelectAddress} />
+      </div>
+      <Button className="font-bold" onClick={handleFindCoffeeSpotsClick}>
+        Find Coffee Spots
+      </Button>
+    </div>
+  );
+};
+
+export default FindCoffeeSpot;
