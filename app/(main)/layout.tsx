225d268a7ea5ca7db5f4c8593143db370f667371
bluesimp1102 <jackismyname7@gmail.com>
2024-03-02 16:03:04 -0800

    #24 change header width

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 import { mainConfig } from '@/config/main';
 import { MainNav } from '@/components/nav/main';
 import { Footer } from '@/components/footer';
+import Link from 'next/link';
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -20,16 +20,13 @@ interface MainLayoutProps {
 export default function MainLayout({ children }: MainLayoutProps): JSX.Element {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 bg-background">
+      <header className="z-40 bg-secondary-dark/50 px-8 md:px-12">
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={mainConfig.mainNav} />
           <nav>
             <Link
               href="/login"
-              className={cn(
-                buttonVariants({ variant: 'secondary', size: 'sm' }),
-                'px-4',
-              )}
+              className={cn(buttonVariants({ size: 'sm' }), 'px-4')}
             >
               Login
             </Link>
