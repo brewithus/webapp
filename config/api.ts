9cd98008707297cfd197226b42877c287e68d9ee
bluesimp1102 <jackismyname7@gmail.com>
2024-05-08 09:48:21 -0700

    fix(api): add fallback for api token

--- config/api.ts
+++ config/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5050';
-export const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN ?? '';
+export const API_TOKEN =
+  process.env.NEXT_PUBLIC_API_TOKEN ?? process.env.API_TOKEN ?? '';
 
 export const apiClient = axios.create({
   baseURL: API_URL,
